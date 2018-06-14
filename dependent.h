c8603c56db609db0c170db8e62c0a98b5d3822a3
tender_bum <brickmen75@gmail.com>
2018-06-14 22:57:33 +0300

    Basic vector implemented.

--- dependent.h
+++ dependent.h
@@ -2,8 +2,11 @@
 #define _DEPENDENT_LIB_H_
 
 #include <cstdint>
+#include <limits>
+#include <memory>
 #include <tuple>
 #include <type_traits>
+#include <cassert>
 
 namespace dependent_lib {
 
@@ -14,13 +17,11 @@ using bigest_size_type =
     std::tuple_element_t<std::tuple_size<size_types>::value - 1, size_types>;
 static_assert(sizeof(bigest_size_type) >= sizeof(size_t), "");
 
-template <typename T> constexpr size_t select_size_type() {
-  if (sizeof(T) <= sizeof(int8_t))
-    return 0;
-  if (sizeof(T) <= sizeof(int16_t))
-    return 1;
-  if (sizeof(T) <= sizeof(int32_t))
-    return 2;
+template <typename T>
+constexpr std::size_t select_size_type() {
+  if (sizeof(T) <= sizeof(int8_t)) return 0;
+  if (sizeof(T) <= sizeof(int16_t)) return 1;
+  if (sizeof(T) <= sizeof(int32_t)) return 2;
   static_assert(std::tuple_size<size_types>::value == 4, "");
   return 3;
 }
@@ -28,8 +29,96 @@ template <typename T> constexpr size_t select_size_type() {
 template <typename T>
 using size_type_t = std::tuple_element_t<select_size_type<T>(), size_types>;
 
-} // namespace detail
+template <typename T>
+std::ptrdiff_t required_space_in_types(std::ptrdiff_t size) {
+  if (size < std::numeric_limits<size_type_t<T>>::max()) {
+    return size + 1;
+  }
+  std::terminate();
+}
 
-} // namespace dependent_lib
+template <typename I, typename Alloc>
+void destroy(I f, I l, Alloc a) {
+  using ri = std::reverse_iterator<I>;
+  for (ri it{l}; it != ri{f}; ++it)
+    std::allocator_traits<Alloc>::destroy(a, it.base());
+}
 
-#endif // _DEPENDENT_LIB_H_
+}  // namespace detail
+
+template <typename T, typename Alloc>
+class vector {
+ public:
+  using allocator_type = Alloc;
+  using alloc_traits = std::allocator_traits<Alloc>;
+  using pointer = typename alloc_traits::pointer;
+  using size_type = std::ptrdiff_t;
+  using iterator = T*;
+  using const_iterator = const T*;
+  using reverse_iterator = std::reverse_iterator<T*>;
+
+ private:
+  std::ptrdiff_t size_offset() const noexcept { return 1; }
+
+  detail::size_type_t<T>& small_size() {
+    return *reinterpret_cast<detail::size_type_t<T>*>(data_);
+  }
+
+  const detail::size_type_t<T>& small_size() const {
+    return *reinterpret_cast<const detail::size_type_t<T>*>(data_);
+  }
+
+  pointer data_;
+
+ public:
+  template <typename I>
+  vector(I f, I l, allocator_type a) {
+    auto dist = std::distance(f, l);
+    auto space_in_types = detail::required_space_in_types<T>(dist);
+    auto allocated_memory = space_in_types * sizeof(T);
+    data_ = alloc_traits::allocate(a, allocated_memory);
+    small_size() = dist;
+
+    T* t_begin = &*data_ + (space_in_types - dist);
+    T* t_cur = t_begin;
+    try {
+      for (; f != l; ++f, ++t_cur) alloc_traits::construct(a, t_cur, *f);
+    } catch (...) {
+      detail::destroy(t_begin, t_cur, a);
+      alloc_traits::deallocate(a, data_, allocated_memory);
+    }
+  }
+
+  ~vector() {
+
+  }
+
+  void destroy(allocator_type a) {
+    detail::destroy(begin(), end(), a);
+    alloc_traits::deallocate(a, data_, (size() + size_offset()) * sizeof(T));
+    data_ = nullptr;
+  }
+
+  friend bool operator<(const vector& x, const vector& y) {
+    return std::lexicographical_compare(x.begin(), x.end(), y.begin(), y.end());
+  }
+
+  iterator begin() { return &*data_ + size_offset(); }
+
+  iterator end() { return begin() + size(); }
+
+  const_iterator begin() const { return &*data_ + size_offset(); }
+
+  const_iterator end() const { return begin() + size(); }
+
+  reverse_iterator rbegin() { return end(); }
+
+  reverse_iterator rend() { return begin(); }
+
+  size_type size() const noexcept { return small_size(); }
+
+};
+
+}  // namespace dependent_lib
+
+#endif  // _DEPENDENT_LIB_H_
--- dependent_unittests.cpp
+++ dependent_unittests.cpp
@@ -1,11 +1,13 @@
 #include "catch.h"
 #include "dependent.h"
 
+#include <set>
 #include <iostream>
+#include <scoped_allocator>
 
 namespace {
 
-TEST_CASE("size_type", "[meta]") {
+TEST_CASE("size_type", "[dependent_lib]") {
   using namespace dependent_lib::detail;
 
   static_assert(std::is_same<size_type_t<uint8_t>, int8_t>::value, "");
@@ -18,5 +20,28 @@ TEST_CASE("size_type", "[meta]") {
   };
   static_assert(std::is_same<size_type_t<big>, int64_t>::value, "");
 }
+
+TEST_CASE("smoking_test", "[dependent_lib]") {
+  short arr[] = {1, 2, 6, 2, 6};
+
+  // init 1 vector
+  {
+    std::allocator<short> a{};
+    dependent_lib::vector<short, std::allocator<short>> dv(std::begin(arr),
+                                                           std::end(arr), a);
+    REQUIRE(dv.size() == 5);
+    dv.destroy(a);
+  }
+  {
+    using allocator = std::scoped_allocator_adaptor<std::allocator<short>>;
+    using vec_t = dependent_lib::vector<short, allocator>;
+    std::set<vec_t, std::less<>, allocator> s;
+    static_assert(std::uses_allocator<vec_t, allocator>::value, "");
+
+    s.emplace(std::begin(arr), std::end(arr));
+  }
+}
+
+// TODO: tests
 
 }  // namespace
