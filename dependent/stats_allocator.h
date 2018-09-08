292f66486d582e271c42a45bd31aae14b8f29f7a
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2018-09-08 21:47:30 +0100

    Issue #3: Implementing an allocator to collect stats about malloc usage

---
+++ dependent/stats_allocator.h
@@ -0,0 +1,98 @@
+#ifndef _DEPENDEDENT_STATS_ALLOCATOR_H_
+#define _DEPENDEDENT_STATS_ALLOCATOR_H_
+
+#include <cstddef>
+#include <memory>
+#include <type_traits>
+#include <utility>
+
+// Wrapper for std::allocator to collect some memory stats.
+// (total memory usage, memory usage for specific types etc)
+//
+// The metrics counting is not thread safe.
+
+namespace dependent {
+
+namespace detail {
+
+// Stats for single type.
+// Stats for a single type are linked into an intrusive list for an area.
+struct memory_stats_for_t {
+  std::size_t allocated_size = 0u;
+  const memory_stats_for_t* next = nullptr;
+
+  memory_stats_for_t(const memory_stats_for_t*& list)
+      : next{std::exchange(list, this)} {}
+};
+
+}  // namespace detail
+
+template <typename Tag>
+class area_stats {
+  using t_stats = detail::memory_stats_for_t;
+
+  // Global list of stats for a single area.
+  static const t_stats*& stats_list() {
+    static const t_stats* r;
+    return r;
+  }
+
+  template <typename T>
+  static t_stats& stats_for_t() {
+    static t_stats r{stats_list()};
+    return r;
+  }
+
+ public:
+  template <typename T>
+  static std::size_t allocated_size_for_t() {
+    return stats_for_t<T>().allocated_size;
+  }
+
+  static std::size_t total_allocated_size() {
+    std::size_t res = 0;
+    for (const t_stats* head = stats_list(); head; head = head->next) {
+      res += head->allocated_size;
+    }
+    return res;
+  }
+
+  template <typename T>
+  static void report_allocation(std::size_t size) {
+    stats_for_t<T>().allocated_size += size;
+  }
+
+  template <typename T>
+  static void report_deallocation(std::size_t size) {
+    stats_for_t<T>().allocated_size -= size;
+  }
+};
+
+template <typename T, typename Tag>
+class stats_allocator {
+  using global_stats = area_stats<Tag>;
+
+  using std_traits = std::allocator_traits<std::allocator<T>>;
+
+ public:
+  using value_type = T;
+  using propagate_on_container_move_assignment = std::true_type;
+
+  using pointer = typename std_traits::pointer;
+  using size_type = typename std_traits::size_type;
+  using const_void_pointer = typename std_traits::const_void_pointer;
+
+  static pointer allocate(size_type n, const_void_pointer hint = 0) {
+    global_stats::template report_allocation<T>(n * sizeof(T));
+    return std::allocator<T>().allocate(n, hint);
+  }
+
+  static void deallocate(pointer p, size_type n) {
+    global_stats::template report_deallocation<T>(n * sizeof(T));
+    std::allocator<T>().deallocate(p, n);
+  }
+};
+
+}  // namespace dependent
+
+#endif  // _DEPENDEDENT_STATS_ALLOCATOR_H_
---
+++ dependent/ut/stats_allocator_ut.cpp
@@ -0,0 +1,102 @@
+#include "dependent/stats_allocator.h"
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "catch/catch.h"
+
+namespace {
+
+template <template <typename...> class A, typename... TailArgs>
+void type_checks() {
+  using traits = std::allocator_traits<A<int, TailArgs...>>;
+
+  static_assert(std::is_same_v<int, typename traits::value_type>);
+  static_assert(std::is_same_v<int*, typename traits::pointer>);
+  static_assert(std::is_same_v<const int*, typename traits::const_pointer>);
+
+  static_assert(std::is_same_v<void*, typename traits::void_pointer>);
+  static_assert(
+      std::is_same_v<const void*, typename traits::const_void_pointer>);
+
+  static_assert(
+      std::is_same_v<std::ptrdiff_t, typename traits::difference_type>);
+  static_assert(std::is_same_v<std::size_t, typename traits::size_type>);
+
+  static_assert(!traits::propagate_on_container_copy_assignment::value);
+  static_assert(traits::propagate_on_container_move_assignment::value);
+  static_assert(!traits::propagate_on_container_swap::value);
+  static_assert(traits::is_always_equal::value);
+
+  static_assert(std::is_same_v<A<char, TailArgs...>,
+                               typename traits::template rebind_alloc<char>>);
+}
+
+template <typename Tag>
+struct stats_containers {
+  template <typename T>
+  using vector = std::vector<T, dependent::stats_allocator<T, Tag>>;
+
+  template <typename Key, typename T, typename Hash = std::hash<Key>,
+            typename KeyEqual = std::equal_to<Key>>
+  using unordered_map = std::unordered_map<
+      Key, T, Hash, KeyEqual,
+      dependent::stats_allocator<std::pair<const Key, T>, Tag>>;
+
+  template <typename Char, typename CharTraits = std::char_traits<Char>>
+  using basic_string = std::basic_string<Char, CharTraits,
+                                         dependent::stats_allocator<Char, Tag>>;
+
+  using string = basic_string<char>;
+};
+
+}  // namespace
+
+TEST_CASE("type_checks", "[stats_allocator, dependent_lib]") {
+  // Sanity check
+  type_checks<std::allocator>();
+
+  // Actual test
+  struct tag {};
+  type_checks<dependent::stats_allocator, tag>();
+}
+
+TEST_CASE("std_vector_reserve", "[stats_allocator, dependent_lib]") {
+  struct tag {};
+  using stats = dependent::area_stats<tag>;
+
+  REQUIRE(stats::total_allocated_size() == 0u);
+
+  {
+    stats_containers<tag>::vector<int> v(100);
+    REQUIRE(stats::total_allocated_size() == v.capacity() * sizeof(int));
+    REQUIRE(stats::allocated_size_for_t<int>() == v.capacity() * sizeof(int));
+  }
+
+  REQUIRE(stats::total_allocated_size() == 0u);
+}
+
+TEST_CASE("std_unordered_map_of_strings", "[stats_allocator, dependent_lib]") {
+  struct tag {};
+  using stats = dependent::area_stats<tag>;
+
+  stats_containers<tag>::unordered_map<stats_containers<tag>::string,
+                                       stats_containers<tag>::string>
+      m;
+
+  REQUIRE(stats::total_allocated_size() == 0);
+
+  m["short"] = "short";  // short string
+
+  REQUIRE(stats::total_allocated_size() > 0);         // unordered map staff
+  REQUIRE(stats::allocated_size_for_t<char>() == 0);  // strings are short;
+
+  auto& entry = m["short"];
+  entry = "loooooooooooooooooooooooooooooooooooooong string";
+
+  // I think that +1 has to do with '\0' - since you can't override it.
+  // reading libc++ code is quite hard in this area, so I'm not sure.
+  REQUIRE(stats::allocated_size_for_t<char>() == entry.capacity() + 1);
+}
