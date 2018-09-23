4c016d2b76fa15d76709d9a1ea4259ec34f697f6
ofats <ogfatog@gmail.com>
2018-09-23 19:00:29 +0300

    Issue #10: Adding limited_string type

--- dependent/dependent.h
+++ dependent/dependent.h
@@ -11,78 +11,13 @@
 #include <type_traits>
 
 #include "dependent/future_std_stubs.h"
+#include "dependent/meta.h"
 
 namespace dependent_lib {
 
 // This is a cusomization point for the prefered span.
 template <typename T>
 using span = future_std_stubs::span<T>;
-
-namespace detail {
-
-template <typename...>
-using void_t = void;
-
-template <typename I>
-using iterator_category_t = typename std::iterator_traits<I>::iterator_category;
-
-template <typename T, typename A, typename = void>
-struct DependentTypeImpl : std::false_type {};
-
-template <typename T, typename A>
-struct DependentTypeImpl<                                                   //
-    T, A,                                                                   //
-    void_t<decltype(std::declval<T&>().destroy(std::declval<const A&>()))>  //
-    > : std::uses_allocator<T, A> {};                                       //
-
-template <typename I, typename = void>
-struct ForwardIteratorImpl : std::false_type {};
-
-template <typename I>
-struct ForwardIteratorImpl<
-    I,  //
-    std::enable_if_t<std::is_base_of<std::forward_iterator_tag,
-                                     iterator_category_t<I>>::value>>
-    : std::true_type {};
-
-template <typename R, typename = void>
-struct ForwardRangeImpl : std::false_type {};
-
-template <typename R>
-struct ForwardRangeImpl<R, void_t<decltype(std::begin(std::declval<R>()),
-                                           std::end(std::declval<R>()))>>
-    : ForwardIteratorImpl<decltype(std::begin(std::declval<R>()))> {};
-
-template <typename R>
-constexpr auto forward_begin(std::remove_reference_t<R>& r) {
-  return std::begin(r);
-}
-
-template <typename R>
-auto forward_begin(std::remove_reference_t<R>&& r) {
-  return std::make_move_iterator(std::begin(r));
-}
-
-template <typename R>
-auto forward_end(std::remove_reference_t<R>& r) {
-  return std::end(r);
-}
-
-template <typename R>
-auto forward_end(std::remove_reference_t<R>&& r) {
-  return std::make_move_iterator(std::end(r));
-}
-
-}  // namespace detail
-
-template <typename T, typename A>
-constexpr bool DependentType = detail::DependentTypeImpl<T, A>::value;
-
-template <typename I>
-constexpr bool ForwardIterator = detail::ForwardIteratorImpl<I>::value;
-
-template <typename R>
-constexpr bool ForwardRange = detail::ForwardRangeImpl<R>::value;
 
 namespace detail {
 
